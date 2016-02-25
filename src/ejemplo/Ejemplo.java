a0dbbaf0064e005faf546a86e9fcf7e9562c55dd
Usuario10 <Usuario10@FI4531119-10.unal.loc>
2016-02-25 03:29:05 -0500

    new commit2235

---
+++ src/ejemplo/Ejemplo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author Usuario10
+ */
+public class Ejemplo {
+String tittle;
+    public Ejemplo(String tittle) {
+    this.tittle=tittle;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
